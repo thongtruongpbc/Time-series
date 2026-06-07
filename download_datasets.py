41fc1056045ddbdc36451a5b37c42d6b5c32fc80
Trương Xuân Thông <144106916+thongtruongpbc@users.noreply.github.com>
2026-06-07 19:10:03 +0700

    Update download_datasets.py

--- download_datasets.py
+++ download_datasets.py
@@ -1,6 +1,3 @@
-# Refs: https://github.com/JovianHQ/opendatasets
-# gg drive for datasets: https://drive.usercontent.google.com/download?id=1m8jh1z4VNMgQ49DRwywyvYYgs3G5WBsB&export=download&authuser=0
-# can use: gdown --fuzzy "https://drive.google.com/file/d/1m8jh1z4VNMgQ49DRwywyvYYgs3G5WBsB/view"
 
 import gdown
 import os
@@ -8,8 +5,8 @@ import os
 file_id = "1m8jh1z4VNMgQ49DRwywyvYYgs3G5WBsB"
 
 gdown.download(
-    id=file_id, output="/home/cds/mnt/thongtx/datasets/dataset.zip", quiet=False
+    id=file_id, output="./datasets/dataset.zip", quiet=False
 )
 
-gdown.extractall("/home/cds/mnt/thongtx/datasets/dataset.zip")
-os.remove("/home/cds/mnt/thongtx/datasets/dataset.zip")
+gdown.extractall("./datasets/dataset.zip")
+os.remove("./datasets/dataset.zip")
